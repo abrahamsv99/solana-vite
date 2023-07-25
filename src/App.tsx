761caca290c9a42ef1d70a24c9870d3142380889
Abrahamsv99 <104535591+Abrahamsv99@users.noreply.github.com>
2023-07-25 11:44:19 -0600

    Update to wallet function

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@
 import { ContextProvider } from './contexts/ContextProvider';
 import { AppBar } from './commponents/AppBar';
 import Notifications from './commponents/Notification'
-import { SomeComponent } from './commponents/SomeComponent';
 import '@solana/wallet-adapter-react-ui/styles.css'
 import './index.css'
+import { UnityGame } from './commponents/UnityGame';
 // require('@solana/wallet-adapter-react-ui/styles.css');
 // require('../styles/globals.css');
 
@@ -15,7 +15,7 @@ const App: React.FC = () => {
         <div className="flex flex-col h-screen">
           <Notifications />
           <AppBar />
-          <SomeComponent />
+          <UnityGame />
         </div>
       </ContextProvider>
     </>
--- src/commponents/UnityGame.tsx
+++ src/commponents/UnityGame.tsx
@@ -1,12 +1,30 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Fragment, useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 export const UnityGame = () => {
-  const { unityProvider } = useUnityContext({
+  const { connection } = useConnection();
+  const wallet = useWallet();
+
+  const { unityProvider, sendMessage, isLoaded } = useUnityContext({
     loaderUrl: "build/Builds.loader.js",
     dataUrl: "build/Builds.data",
     frameworkUrl: "build/Builds.framework.js",
     codeUrl: "build/Builds.wasm",
   });
 
-  return <Unity unityProvider={unityProvider} />;
+  useEffect(() => {
+    if (wallet.publicKey && isLoaded) {
+      sendMessage('WalletHandler', 'OnWalletConnected', wallet.publicKey.toString())
+      console.log(wallet.publicKey)
+    }
+  }, [wallet, isLoaded])
+
+  return (
+    <div >
+      <div>
+        <Unity unityProvider={unityProvider} style={{width: 800, height: 600}} devicePixelRatio={window.devicePixelRatio} className={`${wallet.publicKey ? " " : "hidden"}`} />
+      </div>
+    </div>
+  );
 }
